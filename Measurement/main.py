145005df3c616176a5449da5e5ff40289032e516
UsherKhan <40347841+UsherKhan@users.noreply.github.com>
2023-05-31 02:25:31 +0500

    Update main.py
    Improvements made:
    
    Removed unnecessary imports and variables.
    Simplified the log file handling.
    Used a global variable to control the measurement loop.
    Updated the route names for starting and stopping the measurement.
    Added an endpoint to retrieve the measurement logs.

--- Measurement/main.py
+++ Measurement/main.py
@@ -1,86 +1,65 @@
 import time
-import asyncio
 import requests
-import logging
-from threading import Thread, Lock
 from flask import Flask, request
-import os
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.INFO)
-config=dict(os.environ)
 app = Flask(__name__)
-f=open(config['logs'],'w')
-run_lock=Lock()
-stop=False
-worker=''
+log_file = 'rtt_logs.txt'  # Name of the log file
+stop_measurement = False  # Flag to control the measurement loop
 
-def create_log_entry(epoch,event,rtt):
-	print("{epoch}::{event}::{RTT}".format(epoch=epoch,event=event,RTT=rtt))
-	f.write("{epoch}::{event}::{RTT}\n".format(epoch=epoch,event=event,RTT=rtt))
-	return 0
+def create_log_entry(epoch, event, rtt):
+    """
+    Creates a log entry with the given epoch, event, and round-trip time (RTT).
+    Appends the log entry to the log file.
+    """
+    log_entry = f"{epoch}::{event}::{rtt}\n"
+    with open(log_file, 'a') as f:
+        f.write(log_entry)
 
-def measure_rtt(event,sample_density):
-	while(True):
-		start=time.time()
-		requests.get(config['victim'])
-		end=time.time()
-		create_log_entry(start,event,end-start)
-		run_lock.acquire()
-		global stop
-		if stop:
-			run_lock.release()
-			return 0
-		run_lock.release()
-		time.sleep(sample_density)
-	return 0
+def measure_rtt(event, sample_density):
+    """
+    Measures the round-trip time (RTT) by sending requests to the web server.
+    The measurement loop runs until the stop_measurement flag is set to True.
+    The RTT is calculated and logged for each measurement.
+    """
+    while not stop_measurement:
+        start_time = time.time()
+        requests.get(request.host_url)
+        end_time = time.time()
+        rtt = end_time - start_time
+        create_log_entry(start_time, event, rtt)
+        time.sleep(sample_density)
 
-@app.route("/On/Start",methods=["GET"])
-@app.route("/Off/Start",methods=["GET"])
-@app.route("/On/Iteration",methods=["GET"])
-@app.route("/Off/Iteration",methods=["GET"])
-def log_event():
-	global worker
-	run_lock.acquire()
-	global stop
-	stop = True
-	run_lock.release()
-	if worker!='':
-		worker.join()
-	#asyncio.create_task(create_log_entry(time.time(),request.path,-1))
-	create_log_entry(time.time(),request.path,-1)
-	return "{success:True}"
+@app.route("/start", methods=["GET"])
+def start_measurement():
+    """
+    Starts the RTT measurement.
+    Retrieves the event and sample density from the request parameters.
+    Calls the measure_rtt function with the provided parameters.
+    """
+    global stop_measurement
+    stop_measurement = False
+    event = request.args.get('event', 'Measurement')
+    sample_density = float(request.args.get('sample_density', 1.0))
+    measure_rtt(event, sample_density)
+    return "Measurement started"
 
-@app.route("/On/Idle",methods=["GET"])
-@app.route("/Off/Idle",methods=["GET"])
-@app.route("/On/Burst",methods=["GET"])
-@app.route("/Off/Noise",methods=["GET"])
-def measure_event():
-	global worker
-	run_lock.acquire()
-	global stop
-	stop = True
-	run_lock.release()
-	if worker!='':
-		worker.join()
-	run_lock.acquire()
-	worker=Thread(target=measure_rtt,args=(request.path,int(config['sample_density']),))
-	stop = False
-	run_lock.release()
-	worker.start()
-	return "{success:True}"
+@app.route("/stop", methods=["GET"])
+def stop_measurement():
+    """
+    Stops the RTT measurement by setting the stop_measurement flag to True.
+    """
+    global stop_measurement
+    stop_measurement = True
+    return "Measurement stopped"
 
-@app.route("/end",methods=["GET"])
-def end():
-	global worker
-	run_lock.acquire()
-	global stop
-	stop = True
-	run_lock.release()
-	if worker!='':
-		worker.join()
-	f.close()
-	return "{success:True}"
+@app.route("/logs", methods=["GET"])
+def get_logs():
+    """
+    Retrieves the contents of the log file and returns it as the response.
+    """
+    with open(log_file, 'r') as f:
+        logs = f.read()
+    return logs
 
 if __name__ == "__main__":
-	app.run(debug=True,host="0.0.0.0",port=config['port'])
+    app.run(debug=True, host="0.0.0.0", port=8080)
